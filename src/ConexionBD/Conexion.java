f2f9f0f401a7ea34b00bebfc8461c88831a76e9b
ChristianYael <chrisyael240205@gmail.com>
2025-12-10 00:11:10 -0700

    a chambear

--- src/ConexionBD/Conexion.java
+++ src/ConexionBD/Conexion.java
@@ -4,10 +4,29 @@
  */
 package ConexionBD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Player
  */
 public class Conexion {
-
+    private static String url = "jdbc:sqlserver://DESKTOP-QUA9QDT;databaseName=BDZoologico;encrypt=false"; 
+    private static String user = "sa";
+    private static String password = "123";
+    private static Connection con = null;
+    
+    // CREATE, READ, UPDATE, DELETE
+    // CREAR, LEER, ACTUALIZAR, BORRAR
+    
+    public static Connection getConnection(){
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            System.getLogger(Conexion.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+        return con;
+    }//getConnection
 }
